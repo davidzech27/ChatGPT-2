fc6b9aec60ec63214c7c80e85a53480678381452
David Zechowy <davidzech27@gmail.com>
2023-03-06 02:41:03 -0800

    fix eslint errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,10 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
-	overrides: [
-		{
-			extends: ["plugin:@typescript-eslint/recommended-requiring-type-checking"],
-			files: ["*.ts", "*.tsx"],
-			parserOptions: {
-				project: "tsconfig.json",
-			},
-		},
-	],
 	parser: "@typescript-eslint/parser",
 	parserOptions: {
 		project: "./tsconfig.json",
 	},
-	plugins: ["@typescript-eslint"],
-	extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
-	rules: {
-		"@typescript-eslint/consistent-type-imports": [
-			"warn",
-			{
-				prefer: "type-imports",
-				fixStyle: "inline-type-imports",
-			},
-		],
-	},
+	extends: ["next/core-web-vitals"],
 }
 
 module.exports = config
